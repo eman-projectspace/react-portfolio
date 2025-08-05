2d512ad35258034fdbca2b7c58a07509075a8982
eman-projectspace <emanwaheed001@gmail.com>
2025-08-05 16:10:21 +0500

    Changed Profile Picture

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ const About = () => {
         viewport={{ once: true }}
       >
         <motion.img
-          src="/Eman.webp"
+          src="/images-profile.png"
           alt="Eman"
           className="w-48 h-48 md:w-64 md:h-64 object-cover rounded-full border-4 border-blue-700 shadow-lg"
           initial={{ scale: 0.8, opacity: 0 }}
