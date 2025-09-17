4449325cbada5cf7f5cb3f8436e73c6c219f23d2
eman-projectspace <emanwaheed001@gmail.com>
2025-09-18 01:34:51 +0500

    Remove BG code from each page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -90,7 +90,7 @@ const Blog = () => {
   return (
     <section className="min-h-screen text-white px-6 py-20">
       <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-[3fr_1fr] gap-10 relative">
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
         {/* LEFT: Blog posts */}
         <div>
           <SmallDots />
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -213,7 +213,7 @@ const Projects = () => {
 
   return (
     <section className="min-h-screen text-white flex flex-col items-center justify-center text-center overflow-x-hidden px-6 py-20">
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
       {/* Animated Background */}
       <SmallDots />
 
